f65d33984f8d41cf7ae7ca4727ac7fc8e3f3fb3b
Jackson Levitt <47047592+crazyjackel@users.noreply.github.com>
2022-01-29 15:24:32 -0500

    Stage Changes part 1

---
+++ Assets/Scripts/UI/OverlayView.cs
@@ -0,0 +1,13 @@
+using RedMoon.ReactiveKit;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class OverlayView : View<OverlayViewModel>
+{
+    public override void OnActivation(OverlayViewModel viewModel, CompositeDisposable disposable)
+    {
+    }
+}
---
+++ Assets/Scripts/UI/OverlayViewModel.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RedMoon.ReactiveKit;
+
+public class OverlayViewModel : ViewModel<OverlayViewModel>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
